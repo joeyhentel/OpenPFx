1e1caeead237a37166e090fdddb54fd970ab1187
joeyhentel <joeyhentel@gmail.com>
2024-11-10 14:22:44 -0500

    added icd10 labeling prompt and made the variables 11/10

--- jh_main/jh_pfx_prompts.py
+++ jh_main/jh_pfx_prompts.py
@@ -12,7 +12,7 @@ EXAMPLE = """
 
 """
 
-BASELINE_ZEROSHOT_PROMPT = """
+BASELINE_ZEROSHOT_INSTRUCTION = """
 
 <Prompt>
 Please generate new <PFx> for the <Incidental Finding>
@@ -37,7 +37,7 @@ Additional Instructions:
 # INCORPORATE ZEROSHOT_REFLEXION_READING_LEVEL
 
 
-SINGLE_FEWSHOT_PROMPT = """
+SINGLE_FEWSHOT_INSTRUCTION = """
 
 <Prompt>
 Please generate new <PFx> for the <Incidental Finding>
@@ -63,7 +63,7 @@ Additional Instructions:
 """
 
 
-MULTIPLE_FEWSHOT_PROMPT = """
+MULTIPLE_FEWSHOT_INSTRUCTION = """
 Please generate 5 new <PFx> for the <Incidental Finding> 
 
 Output should be formatted as a json with the following attributes/fields: finding, ICD10_code, PFx, PFx_ICD10_code 
@@ -85,6 +85,79 @@ Additional Instructions:
 {Incidental_Finding} 
 </Incidental Finding>  
 """
+
+ICD10_EXAMPLE = """ <PFx>
+{PFx}
+</PFx>
+<PFx_ICD10_code>
+'''{{"PFx_ICD10_code":{PFx_ICD10_code}"}}'''
+</PFx_ICD10_code>
+
+"""
+
+SINGLE_FEWSHOT_ICD10_LABELING_INSTRUCTION = """<Prompt>
+Using the patient friendly explanations {PFx} in <Examples> as well as their associated ICD10 codes <ICD10>, please generate a new <ICD10> for the {PFx}
+
+Output should be formatted as a json with the following attribute/field: ICD10_code 
+</Prompt>
+
+<Examples>
+{examples} 
+</Examples>
+
+<PFx>
+{PFx}
+</PFx>
+
+"""
+
+MULTIPLE_FEWSHOT_ICD10_LABELING_INSTRUCTION = """
+Using the patient friendly explanations {PFx} in <Examples> as well as their associated ICD10 codes <ICD10>, please generate a new <ICD10> for each of the five {PFx}
+
+Output should be formatted as a json with the following attribute/field: ICD10_code 
+</Prompt>
+
+<Examples>
+{examples} 
+</Examples>
+
+<PFx>
+{PFx}
+</PFx>
+
+
+"""
+
+example = PromptTemplate(
+    input_variables = ["Incidental_Finding", "ICD10_code", "PFx", "PFx_ICD10_code"]
+    template = EXAMPLE,
+    )
+
+icd10_example = PromptTemplate(
+    input_variables = ["PFx_ICD10_code", "PFx"],
+    template = ICD10_EXAMPLE,
+    )
+
+baseline_zeroshot_prompt = PromptTemplate(
+    input_variables = ["Incidental_Finding"],
+    template = BASELINE_ZEROSHOT_INSTRUCTION,
+    )
+
+single_fewshot_prompt = PromptTemplate(
+    input_variables = ["examples", "Incidental_Finding"]
+    template = SINGLE_FEWSHOT_INSTRUCTION,
+    )
+
+single_fewshot_icd10_labeling_prompt = PromptTemplate(
+    input_variables = ["examples", "PFx"]
+    template = SINGLE_FEWSHOT_ICD10_LABELING_INSTRUCTION,
+    )
+
+multiple_fewshot_icd10_labeling_prompt = PromptTemplate(
+    input_variables = ["examples", "PFx"]
+    template = MULTIPLE_FEWSHOT_ICD10_LABELING_INSTRUCTION,
+    )
+
 
 
 
