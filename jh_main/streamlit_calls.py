d0548570b56818f92851f2faa6c5c3d0f0a2a06f
joeyhentel <123221508+joeyhentel@users.noreply.github.com>
2025-08-15 16:03:09 +0000

    fewshot fix

--- jh_main/streamlit_calls.py
+++ jh_main/streamlit_calls.py
@@ -103,7 +103,7 @@ def fewshot_call(finding, code, grade_level, ai_model):
     N_A = "N/A"
 
     # import fewshot examples
-    df_fewshot = pd.read_csv('pfx_fewshot_examples_college.csv')
+    df_fewshot = pd.read_csv('jh_main/pfx_fewshot_examples_college.csv')
 
     # import prompts 
     from jh_pfx_prompts import example, icd10_example, baseline_zeroshot_prompt, single_fewshot_icd10_labeling_prompt
